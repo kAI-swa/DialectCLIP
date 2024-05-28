d035f3f42487bc56da64f201aea413fe29b75412
Kainan Liu <146005327+kAI-swa@users.noreply.github.com>
2024-05-28 22:35:22 +0800

    Delete dialect_clip_withger directory

--- dialect_clip_withger/__init__.py
+++
@@ -1,5 +0,0 @@
-from .configuration_dialectclip import *
-from .trainer_dialectclip import *
-from .modeling_dialectclip import *
-from .utils_dialectclip import *
-from .evaluator_dialectclip import *
